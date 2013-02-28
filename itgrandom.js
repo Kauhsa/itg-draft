8153fab21f75726f47bc87b8a5923a0876310199
Kauhsa <kauhsa@viuhka.fi>
2013-02-28 23:43:59 +0200

    Initial commit

---
+++ itgrandom.js
@@ -0,0 +1,19 @@
+var ITGRandom;
+ITGRandom = (function(){
+  ITGRandom.displayName = 'ITGRandom';
+  var prototype = ITGRandom.prototype, constructor = ITGRandom;
+  function ITGRandom(songdata){
+    this.songdata = songdata;
+  }
+  prototype.songs_between_ratings = function(start_rating, end_rating){
+    var i$, ref$, len$, song, ref1$, results$ = [];
+    for (i$ = 0, len$ = (ref$ = this.songdata).length; i$ < len$; ++i$) {
+      song = ref$[i$];
+      if (start_rating >= (ref1$ = song.rating) && ref1$ >= end_rating) {
+        results$.push(song);
+      }
+    }
+    return results$;
+  };
+  return ITGRandom;
+}());
---
+++ itgrandom.ls
@@ -0,0 +1,7 @@
+class ITGRandom
+    (songdata) ->
+        @songdata = songdata
+
+    songs_between_ratings: (start_rating, end_rating) ->
+        [song for song in @songdata when start_rating >= song.rating >= end_rating]
+
